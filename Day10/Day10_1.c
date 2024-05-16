5fdb19fcb8a70afd6436886435bcf5ba12f00b45
Parth-640 <luharparth111@gmail.com>
2024-05-16 14:43:58 +0530

    Day10

--- Day10/Day10_1.c
+++ Day10/Day10_1.c
@@ -8,8 +8,12 @@
 typedef struct msg{
 	long type;
 	int n1[2];
-	int sum;
 }msg_t;
+
+typedef struct msg2{
+	long type;
+	int sum;
+}msg_t2;
 
 int main(){
 	int mqid, ret, s;
@@ -29,41 +33,42 @@ int main(){
 			scanf("%d",&m1.n1[i]);
 		}
 
-		ret = msgsnd(mqid, &m1, sizeof(m1.n1) ,0);
+		ret = msgsnd(mqid, &m1, sizeof(m1)-sizeof(m1.type) ,0);
 		if (ret < 0){
 			perror("msgsnd() failed");
 			_exit(2);
 		}
 		printf("child (send): msg send: %d\n",ret);
 	
-		msg_t m3;
+		msg_t2 m3;
 
-		ret = msgrcv(mqid, &m3, sizeof(m3),2,0);
+		ret = msgrcv(mqid, &m3, sizeof(m3)-sizeof(m3.type),2,0);
 		printf("child receive ret = %d\n",ret);
 		printf("child sum: %d\n",m3.sum);
 	}
 	else{
 		msg_t m2;
 		printf("parent receiver: wainting for msg..\n");
-		ret = msgrcv(mqid, &m2, sizeof(m2.n1),1,0);
+		ret = msgrcv(mqid, &m2, sizeof(m2)-sizeof(m2.type),1,0);
 		printf("parent (receiver): msg received: %d -- n1=%d n2=%d\n", ret, m2.n1[0], m2.n1[1]);
 		
-		msg_t m4;
+		msg_t2 m4;
 		m4.type=2;
 		m4.sum = m2.n1[0] + m2.n1[1];
 
-		printf("sum: %d\n",m4.sum);
+		printf("parent: sum: %d\n",m4.sum);
 		
-		ret = msgsnd(mqid, &m4, sizeof(m4),0);
+		ret = msgsnd(mqid, &m4, sizeof(m4)-sizeof(m4.type),0);
 		if( ret < 0){
 			perror("msgsnd() failed");
 			_exit(2);
 		}
 		printf("parent send msg: %d\n",ret);
 
+		msgctl(mqid,IPC_RMID, NULL);
+
 		waitpid(ret,&s,0);
 
-		msgctl(mqid,IPC_RMID, NULL);
 	}
 
 	return 0;
